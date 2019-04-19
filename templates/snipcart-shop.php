1a9a81879823dceb9577ca3325580af38284efed
Martin Gartner <martin.gartner@bitego.com>
2019-04-19 13:25:10 +0200

    Enhanced descriptions of snipwire API usage and cleaned code

--- templates/snipcart-shop.php
+++ templates/snipcart-shop.php
@@ -1,29 +1,36 @@
 <?php namespace ProcessWire;
 
 /**
- * SnipWire sample shop parent template for "site-regular" site-profile.
+ * SnipWire sample shop parent template for "regular" site-profile.
  * 
  * The purpose of this template file is to provide a product overview for your Snipcart shop.
+ *
+ * We are using the "regular" site-profile as it offers Markup Regions as its output 
+ * strategy and so it's easy to demonstrate how SnipCart works.
  * 
  */
 
 if (!defined('PROCESSWIRE')) die();
 ?>
 <!--
-This is the mini cart display which shows the "Items in cart" count and the total cart value.
+The mini cart display shows the "Items in cart" count and the "total" cart value.
 
 Notice that the container element has a "snipcart-summary" class. Add the markup you want within 
-this container, then add "snipcart-total-items" and "snipcart-total-price" to the elements that will 
-contain cart information.
+this container, then add "snipcart-total-items" and "snipcart-total-price" classes to the elements
+that will contain cart information.
 
-Wrap the container element with a link having the class "snipcart-checkout" and the cart will pop when your 
-visitors click on it.
+Wrap the container element with a link having the class "snipcart-checkout" and the cart will pop 
+when your visitors click on it.
 
 The complete markup is up to you - it just needs to have the described classes from above included!
+
+More here: https://docs.snipcart.com/getting-started/the-cart
 -->
 <p id="masthead-tagline" class="-uk-text-small -uk-text-muted uk-margin-remove uk-text-center snipcart-summary">
-    <a href="#" class="uk-link-reset snipcart-checkout">
-        <?=ukIcon('cart')?> <span class="uk-badge snipcart-total-items"></span> <span class="snipcart-total-price"></span>
+    <a href="#" class="uk-link-reset snipcart-checkout" aria-label="Shopping cart">
+        <?=ukIcon('cart')?>
+        <span class="uk-badge snipcart-total-items" aria-label="Items in cart"></span>
+        <span class="snipcart-total-price" aria-label="Total"></span>
     </a>
 </p>
 
@@ -77,16 +84,20 @@ function ukProductOverview(PageArray $products) {
         $out .= '        </div>';
         $out .= '        <div class="uk-card-footer">';
         $out .= '            <p>';
+        
                                 // This is the part where we render the Snipcart anchor (buy button)
                                 // with data-item-* attributes required by Snipcart.
                                 // The anchor method is provided by MarkupSnipWire module and can be called 
                                 // via custom API variable: $snipwire->anchor()
         $out .= '               ' . wire('snipwire')->anchor($product, 'Buy now', 'uk-button uk-button-primary');
+        
         $out .= '               <span class="uk-align-right uk-text-primary">';
+        
                                     // Get the formatted product price.
                                     // The getProductPriceFormatted method is provided by MarkupSnipWire module and can be called 
                                     // via custom API variable: $snipwire->getProductPriceFormatted()
         $out .= '                   ' . wire('snipwire')->getProductPriceFormatted($product);
+        
         $out .= '               </span>';
         $out .= '            </p>';
         $out .= '        </div>';
