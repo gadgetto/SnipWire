63397ea26154dd5626b209d4f7e744e20e785c86
Martin Gartner <martin.gartner@bitego.com>
2019-04-22 11:16:56 +0200

    Rewrite of shop template field for better readability

--- templates/snipcart-shop.php
+++ templates/snipcart-shop.php
@@ -31,15 +31,15 @@ that will contain cart information.
 Wrap the container element with a link having the class "snipcart-checkout" and the cart will pop 
 when your visitors click on it.
 
-The complete markup is up to you - it just needs to have the described classes from above included!
+The complete markup is up to you - it just needs to have the described classes included!
 
 More here: https://docs.snipcart.com/getting-started/the-cart
 -->
 <p id="masthead-tagline" class="-uk-text-small -uk-text-muted uk-margin-remove uk-text-center snipcart-summary">
     <a href="#" class="uk-link-reset snipcart-checkout" aria-label="Shopping cart">
         <?=ukIcon('cart')?>
-        <span class="uk-badge snipcart-total-items" aria-label="Items in cart"></span>
-        <span class="snipcart-total-price" aria-label="Total"></span>
+        <span class="uk-badge snipcart-total-items uk-text-middle" aria-label="Items in cart"></span>
+        <span class="snipcart-total-price uk-text-middle" aria-label="Total"></span>
     </a>
 </p>
 
@@ -49,7 +49,7 @@ The content element holds your products catalogue.
 <div id="content">
     <?php
     echo ukHeading1(page()->title, 'divider'); 
-    $products = page()->children('limit=10');
+    $products = page()->children('limit=9');
     echo ukProductOverview($products); 
     ?>
 </div>
@@ -75,12 +75,26 @@ function ukProductOverview(PageArray $products) {
 
     foreach ($products as $product) {
         
-        // We use the first image in snipcart_item_image field
+        // We use the first image in snipcart_item_image field for demo
         $image = $product->snipcart_item_image->first();
-        
-        // Create required product image variant
         $productImageMedium = $image->size(600, 0, array('quality' => 70));
         
+        // This is the part where we render the Snipcart anchor (buy button)
+        // with all data-item-* attributes required by Snipcart.
+        // The anchor method is provided by MarkupSnipWire module and can be called 
+        // via custom API variable: $snipwire->anchor()
+        $options = array(
+            'label' => ukIcon('cart'),
+            'class' => 'uk-button uk-button-primary',
+            'attr' => array('aria-label' => __('Add item to cart')),
+        );
+        $anchor = wire('snipwire')->anchor($product, $options);
+        
+        // Get the formatted product price.
+        // The getProductPriceFormatted method is provided by MarkupSnipWire module and can be called 
+        // via custom API variable: $snipwire->getProductPriceFormatted()
+        $priceFormatted = wire('snipwire')->getProductPriceFormatted($product);
+
         $out .= '<a class="uk-link-reset" href="' . $product->url . '">';
         $out .= '    <div class="uk-card uk-card-default uk-card-hover">';
         $out .= '        <div class="uk-card-media-top">';
@@ -91,28 +105,8 @@ function ukProductOverview(PageArray $products) {
         $out .= '            <p>' . $product->snipcart_item_description . '</p>';
         $out .= '        </div>';
         $out .= '        <div class="uk-card-footer">';
-        $out .= '            <p>';
-        
-                                // This is the part where we render the Snipcart anchor (buy button)
-                                // with data-item-* attributes required by Snipcart.
-                                // The anchor method is provided by MarkupSnipWire module and can be called 
-                                // via custom API variable: $snipwire->anchor()
-                                $options = array(
-                                    'label' => ukIcon('cart'),
-                                    'class' => 'uk-button uk-button-primary',
-                                    'attr' => array('aria-label' => __('Add item to cart')),
-                                );
-        $out .= '               ' . wire('snipwire')->anchor($product, $options);
-        
-        $out .= '               <span class="uk-align-right uk-text-primary">';
-        
-                                    // Get the formatted product price.
-                                    // The getProductPriceFormatted method is provided by MarkupSnipWire module and can be called 
-                                    // via custom API variable: $snipwire->getProductPriceFormatted()
-        $out .= '                   ' . wire('snipwire')->getProductPriceFormatted($product);
-        
-        $out .= '               </span>';
-        $out .= '            </p>';
+        $out .=              $anchor;
+        $out .= '            <span class="uk-align-right uk-text-primary">' . $priceFormatted . '</span>';
         $out .= '        </div>';
         $out .= '    </div>';
         $out .= '</a>';
