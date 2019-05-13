e2ec30dc3b61d6c0cb692b3e549b59c1c9134951
Martin Gartner <martin.gartner@bitego.com>
2019-05-13 19:38:19 +0200

    Restructured module directories

--- ProcessSnipWire/ProcessSnipWire.module.php
+++ ProcessSnipWire/ProcessSnipWire.module.php
@@ -55,9 +55,9 @@ class ProcessSnipWire extends Process implements Module {
      *
      */
     public function __construct() {
-        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ExtendedInstaller.php';
-        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'SnipREST.php';
-        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'CurrencyFormat.php';
+        require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'installer/ExtendedInstaller.php';
+        require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'helpers/CurrencyFormat.php';
+        require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'SnipREST.php';
         parent::__construct();
     }
 
