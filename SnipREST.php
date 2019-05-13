291b6814f4cf80683fdcb1f168484f31a62bff19
Martin Gartner <martin.gartner@bitego.com>
2019-05-13 19:11:23 +0200

    Added methods to fetch orders, customers and performance from Snipcart

--- SnipREST.php
+++ SnipREST.php
@@ -108,6 +108,246 @@ class SnipREST extends WireHttp {
     }
 
     /**
+     * Get the all orders from Snipcart dashboard as array.
+     *
+     * Uses WireCache to prevent reloading Snipcart data on each request.
+     *
+     * @param string $key The array key to be returned
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `offset` (int) Number of results to skip. [default = 0] #required
+     *  - `limit` (int) Number of results to fetch. [default = 20] #required
+     *  - `status` (string) A status criteria for your order collection. (Possible values: InProgress, Processed, Disputed, Shipped, Delivered, Pending, Cancelled)
+     *  - `invoiceNumber` (string) The invoice number of the order to retrieve
+     *  - `placedBy` (string) The name of the person who made the purchase
+     *  - `from` (datetime) Will return only the orders placed after this date
+     *  - '`to` (datetime) Will return only the orders placed before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return mixed False if request failed or orders array or string or integer
+     *
+     */
+    public function getOrders($key = '', $options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        if (!$this->getHeaders()) {
+            $this->error(self::getMessagesText('no_headers'));
+            return false;
+        }
+        if ($forceRefresh) $this->wire('cache')->deleteFor(self::cacheNamespace, self::cacheNamePrefixOrders);
+
+        $allowedOptions = array('offset', 'limit', 'status', 'invoiceNumber', 'placedBy', 'from', 'to');
+        $defaultOptions = array(
+            'offset' => 0,
+            'limit' => 20,
+        );
+        $options = array_merge(
+            $defaultOptions,
+            array_intersect_key(
+                $options, array_flip($allowedOptions)
+            )
+        );
+        $query = '';
+        if (!empty($options)) $query = '?' . http_build_query($options);
+
+        // Segmented orders cache (each query is cached self-contained)
+        $cacheName = self::cacheNamePrefixOrders . '.' . md5($query);
+        
+        // Try to get orders array from cache first
+        $response = $this->wire('cache')->getFor(self::cacheNamespace, $cacheName, $expires, function() use($query) {
+            return $this->getJSON(self::apiEndpoint . self::resourcePathOrders . $query);
+        });
+        return ($key && isset($response[$key])) ? $response[$key] : $response;
+    }
+
+    /**
+     * Get total orders from Snipcart dashboard.
+     *
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `status` (string) A status criteria for your order collection. (Possible values: InProgress, Processed, Disputed, Shipped, Delivered, Pending, Cancelled)
+     *  - `placedBy` (string) The name of the person who made the purchase
+     *  - `from` (datetime) Will return only the orders placed after this date
+     *  - '`to` (datetime) Will return only the orders placed before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return boolean|integer False if request failed or total orders as integer
+     * 
+     */
+    public function getOrdersTotal($options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        $allowedOptions = array('status', 'placedBy', 'from', 'to');
+        // default options can't be overwritten because they are not in allowedOptions!
+        $defaultOptions = array(
+            'offset' => 0,
+            'limit' => 1,
+        );        
+        $options = array_merge(
+            $defaultOptions,
+            array_intersect_key(
+                $options, array_flip($allowedOptions)
+            )
+        );
+        return $this->getOrders('totalItems', $options, $expires = WireCache::expireNever, $forceRefresh = false);
+    }
+
+    /**
+     * Get orders items from Snipcart dashboard.
+     *
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `offset` (int) Number of results to skip. [default = 0] #required
+     *  - `limit` (int) Number of results to fetch. [default = 20] #required
+     *  - `status` (string) A status criteria for your order collection. (Possible values: InProgress, Processed, Disputed, Shipped, Delivered, Pending, Cancelled)
+     *  - `invoiceNumber` (string) The invoice number of the order to retrieve
+     *  - `placedBy` (string) The name of the person who made the purchase
+     *  - `from` (datetime) Will return only the orders placed after this date
+     *  - '`to` (datetime) Will return only the orders placed before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return boolean|integer False if request failed or orders items as array
+     * 
+     */
+    public function getOrdersItems($options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        return $this->getOrders('items', $options, $expires, $forceRefresh);
+    }
+
+    /**
+     * Get the all customers from Snipcart dashboard as array.
+     *
+     * Uses WireCache to prevent reloading Snipcart data on each request.
+     *
+     * @param string $key The array key to be returned
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `offset` (int) Number of results to skip. [default = 0] #required
+     *  - `limit` (int) Number of results to fetch. [default = 20] #required
+     *  - `status` (string) A status criteria for your customers collection. (Possible values: Confirmed = created an account, Unconfirmed = checked out as guests)
+     *  - `email` (string) The email of the customer who placed the order
+     *  - `name` (string) The name of the customer who placed the order
+     *  - `from` (datetime) Will return only the customers created after this date
+     *  - '`to` (datetime) Will return only the customers created before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return mixed False if request failed or customers array or string or integer
+     *
+     */
+    public function getCustomers($key = '', $options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        if (!$this->getHeaders()) {
+            $this->error(self::getMessagesText('no_headers'));
+            return false;
+        }
+        if ($forceRefresh) $this->wire('cache')->deleteFor(self::cacheNamespace, self::cacheNamePrefixCustomers);
+
+        $allowedOptions = array('offset', 'limit', 'status', 'email', 'name', 'from', 'to');
+        $defaultOptions = array(
+            'offset' => 0,
+            'limit' => 20,
+        );
+        $options = array_merge(
+            $defaultOptions,
+            array_intersect_key(
+                $options, array_flip($allowedOptions)
+            )
+        );
+        $query = '';
+        if (!empty($options)) $query = '?' . http_build_query($options);
+
+        // Segmented cache (each query is cached self-contained)
+        $cacheName = self::cacheNamePrefixCustomers . '.' . md5($query);
+        
+        // Try to get array from cache first
+        $response = $this->wire('cache')->getFor(self::cacheNamespace, $cacheName, $expires, function() use($query) {
+            return $this->getJSON(self::apiEndpoint . self::resourcePathCustomers . $query);
+        });
+        return ($key && isset($response[$key])) ? $response[$key] : $response;
+    }
+
+    /**
+     * Get total customers from Snipcart dashboard.
+     *
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `status` (string) A status criteria for your customers collection. (Possible values: Confirmed = created an account, Unconfirmed = checked out as guests)
+     *  - `email` (string) The email of the customer who placed the order
+     *  - `name` (string) The name of the customer who placed the order
+     *  - `from` (datetime) Will return only the customers created after this date
+     *  - '`to` (datetime) Will return only the customers created before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return boolean|integer False if request failed or total customers as integer
+     * 
+     */
+    public function getCustomersTotal($options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        $allowedOptions = array('status', 'email', 'name', 'from', 'to');
+        // default options can't be overwritten because they are not in allowedOptions!
+        $defaultOptions = array(
+            'offset' => 0,
+            'limit' => 1,
+        );        
+        $options = array_merge(
+            $defaultOptions,
+            array_intersect_key(
+                $options, array_flip($allowedOptions)
+            )
+        );
+        return $this->getCustomers('totalItems', $options, $expires = WireCache::expireNever, $forceRefresh = false);
+    }
+
+    /**
+     * Get customers items from Snipcart dashboard.
+     *
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `offset` (int) Number of results to skip. [default = 0] #required
+     *  - `limit` (int) Number of results to fetch. [default = 20] #required
+     *  - `status` (string) A status criteria for your customers collection. (Possible values: Confirmed = created an account, Unconfirmed = checked out as guests)
+     *  - `email` (string) The email of the customer who placed the order
+     *  - `name` (string) The name of the customer who placed the order
+     *  - `from` (datetime) Will return only the customers created after this date
+     *  - '`to` (datetime) Will return only the customers created before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return mixed False if request failed or customers items as array
+     * 
+     */
+    public function getCustomersItems($options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        return $this->getCustomers('items', $options, $expires, $forceRefresh);
+    }
+
+    /**
+     * Get the store performance from Snipcart dashboard as array.
+     *
+     * Uses WireCache to prevent reloading Snipcart data on each request.
+     *
+     * @param array $options An array of filter options that will be sent as URL params:
+     *  - `from` (datetime) Will return only the performance after this date
+     *  - '`to` (datetime) Will return only the performance before this date
+     * @param mixed $expires Lifetime of this cache, in seconds, OR one of the options from $cache->save()
+     * @param boolean $forceRefresh Wether to refresh the settings cache
+     * @return mixed False if request failed or performance array
+     *
+     */
+    public function getPerformance($options = array(), $expires = WireCache::expireNever, $forceRefresh = false) {
+        if (!$this->getHeaders()) {
+            $this->error(self::getMessagesText('no_headers'));
+            return false;
+        }
+        if ($forceRefresh) $this->wire('cache')->deleteFor(self::cacheNamespace, self::cacheNamePrefixPerformance);
+
+        $allowedOptions = array('from', 'to');
+        $defaultOptions = array();
+        $options = array_merge(
+            $defaultOptions,
+            array_intersect_key(
+                $options, array_flip($allowedOptions)
+            )
+        );
+        $query = '';
+        if (!empty($options)) $query = '?' . http_build_query($options);
+
+        // Segmented cache (each query is cached self-contained)
+        $cacheName = self::cacheNamePrefixPerformance . '.' . md5($query);
+        
+        // Try to get array from cache first
+        $response = $this->wire('cache')->getFor(self::cacheNamespace, $cacheName, $expires, function() use($query) {
+            return $this->getJSON(self::apiEndpoint . self::resourcePathDataPerformance . $query);
+        });
+        return $response;
+    }
+
+    /**
      * Snipcart REST API connection test.
      * (uses resourcePathSettingsDomain for test request)
      *
