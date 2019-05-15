3d4b098cd52c18ff02794ea6950aea4cf8edad26
Martin Gartner <martin.gartner@bitego.com>
2019-05-15 12:58:50 +0200

    Added test data for ApexChart

--- assets/scripts/PerformanceChart.js
+++ assets/scripts/PerformanceChart.js
@@ -9,11 +9,41 @@
  *
  */
 
-
-
-
-
 jQuery(document).ready(function($) {
+    var options = {
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      series: [{
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+      }
+    }
 
+    var chart = new ApexCharts(
+      document.querySelector("#snipwire-chart-performance"),
+      options
+    );
+
+    chart.render();
 
 }); 
