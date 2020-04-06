9d51691313057eeacf87bbdb4811afd749fed020
Martin Gartner <martin.gartner@bitego.com>
2020-04-06 18:56:57 +0200

    Added href key to SnipWire module info

--- SnipWire.module.php
+++ SnipWire.module.php
@@ -35,6 +35,7 @@ class SnipWire extends WireData implements Module, ConfigurableModule {
             'summary' => __('Full Snipcart shopping cart integration for ProcessWire.'), // Module Summary
             'version' => '0.8.5',
             'author'  => 'Martin Gartner',
+            'href' => 'https://processwire.com/talk/topic/21554-snipwire-snipcart-integration-for-processwire/',
             'icon' => 'shopping-cart', 
             'singular' => true, 
             'autoload' => true, 
